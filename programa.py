fbf8f35abadfbc1d731c395dda6c4445c50383aa
alepeteporico <tojandro@gmail.com>
2019-03-06 20:19:18 +0100

    Ejercicio 3 terminado

--- programa.py
+++ programa.py
@@ -1,5 +1,56 @@
-with open("overwatch-api-complete-player-profile-example.json") as fichero
-datos=json.load(fichero)
+import json
+with open("overwatch-api-complete-player-profile-example.json") as fichero:
+    datos=json.load(fichero)
+
+lista_win=[]
+lista_vic_derr=[]
+lista_daño=[]
+
+def ejer1(datos):
+    lista_heroes=[]
+    lista_heroes=(datos.get("quickplay").get("heroComparison").get("timePlayed"))
+    for heroe in lista_heroes.keys():
+        print("-",heroe)
+    print("")
+
+
+def ejer2(datos):
+    lista_datos=[]
+    lista_oro=[]
+    medallas=int(input("Dime cuantas medallas especificas: "))
+    lista_datos=(datos["quickplay"]["careerStats"])
+    cont=0
+
+    for campo,valor in lista_datos.items():
+        for c,v in valor.items():
+        
+            if c=="matchAwards":
+
+                lista_oro.append(v.get("medalsGold",0))
+                
+    print(len(lista_oro))
+                    
+    for num in lista_oro:
+            
+        if num>=medallas:
+           cont=cont+1
+
+    print("Has conseguido %d medallas o mas con %d héroes"%(medallas,cont-1))
+
+def ejer3(datos):
+    lista_win=[]
+    
+    victorias=int(input("Dime cuantas victorias especificas: "))
+    lista_win=(datos["quickplay"]["heroComparison"]["gamesWon"])
+    
+    print("Has ganado %s partida/s o mas con:"%(victorias))
+
+    for heroe,win in lista_win.items():
+        if win>=victorias:
+            print("-",heroe)
+    print("")
+    
+
 
 while True:
     print("================================================================================")
@@ -12,3 +63,24 @@ while True:
     print("================================================================================")
     elec=int(input("Elige una opción: "))
     print("")
+
+    if elec==6:
+        break
+    
+    elif elec==1:
+        ejer1(datos)
+        print("------------------------------")
+        intro=input("Pulsa enter para continuar")
+        print("")
+    
+    elif elec==2:
+        ejer2(datos)
+        print("------------------------------")
+        intro=input("Pulsa enter para continuar")
+        print("")
+
+    elif elec==3:
+        ejer3(datos)
+        print("------------------------------")
+        intro=input("Pulsa enter para continuar")
+        print("")
